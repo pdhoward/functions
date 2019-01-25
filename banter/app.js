9de926fd4753dd0121dd2b204682c993ccb5eb94
pdhoward <patrick.howard@hotmail.com>
2019-01-25 14:39:48 -0500

    update

--- banter/app.js
+++ banter/app.js
@@ -1,9 +1,13 @@
-const http = require('http')
+const http =    require('http')
+const uuid =    require('uuid/v4')
+const fn =      require('./package.json')
 const { PORT = 3000 } = process.env
 
 http.createServer((req, res) => {     
   const start = Date.now()
   let obj = {}
+  obj.id = uuid()
+  obj.fn = ''
   obj.time = `Strategic Machine Response time: ${Date.now() - start}ms`
   obj.message = `Hello from Banter`
   res.writeHead(200, {'Content-Type': 'application/json'});
