4aacd35374d2965b995ba5295a79a1f55e5d90d2
pdhoward <patrick.howard@hotmail.com>
2019-01-31 20:09:59 -0500

    updates

--- wat/README.md
+++ wat/README.md
@@ -2,3 +2,5 @@
 
 // https://up.docs.apex.sh/#installation
 
+// https://www.basistech.com/text-analytics/rosette/language-identifier/
+
--- wat/app.js
+++ wat/app.js
@@ -12,35 +12,20 @@ http.createServer((req, res) => {
   });
 
   req.on('end', async () => {
-    //////////////////////////////
-    let response = []
-    let obj = {}
-    obj.id = uuid()
-    obj.fn = fn.name
-    obj.timeStamp = Date.now()
-    obj.message = `THis worked well at ${start}`
-    response.push(obj)
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(response));
-    res.end();
-
-
-
-    //////////////////////////////
-
-    /*
+    
     try {   
       
-      const reply = await main(JSON.parse(conversation));
-      let response = []
+      const reply = await main(JSON.parse(conversation));      
       let obj = {}
+      let cloned = {}
       obj.id = uuid()
       obj.fn = fn.name
       obj.timeStamp = Date.now()
 
      let response =  reply.map((r) => {
           obj.message = r 
-          return obj
+          cloned = {...obj}
+          return cloned
       })
          
       res.writeHead(200, {'Content-Type': 'application/json'});
@@ -51,7 +36,7 @@ http.createServer((req, res) => {
       res.writeHead(500, {'Content-Type': 'application/json'});
       res.write(JSON.stringify(error));
     }
-    */
+    
     res.end();
   });
   
@@ -73,8 +58,8 @@ async function main(conversation) {
 const getMessage = (conversation, cb) => {
   const response = []; 
 
-  response.push(`Your message read ' ${conversation.Body} '`)
-  response.push(`Please try again. I am only ${conversation.confidence} that I understood your message`)
+  response.push(`Your message read '${conversation.Body}'`)
+  response.push(`Please try again. I am only ${conversation.confidence} confident that I understood your message`)
 
   cb(response);
 };
